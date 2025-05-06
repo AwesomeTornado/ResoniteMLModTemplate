caf0f62bef0675ef3d87b128d88e5c1e5da12930
Awesome_tornado_ <73045075+AwesomeTornado@users.noreply.github.com>
2025-05-06 12:00:54 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("HostCrashGuard")]
+[assembly: AssemblyProduct("HostCrashGuard")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCompany("__Choco__")]
+[assembly: AssemblyCopyright("Copyright © 2025 AwesomeTornado")]
+[assembly: AssemblyVersion(MonkeyLoader.HostCrashGuard.AssemblyInfo.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(MonkeyLoader.HostCrashGuard.AssemblyInfo.VERSION_CONSTANT)]
